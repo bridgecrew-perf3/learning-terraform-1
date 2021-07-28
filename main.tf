84bc3045d0bbc6807a93a7eda159e309fed17bc0
Salim Rashid <salimrashid1990@gmail.com>
2021-07-28 13:34:16 +0300

    Added a splat expression * for container_name output resource.

--- main.tf
+++ main.tf
@@ -42,22 +42,22 @@ resource "docker_container" "nodered_container" {
 # }
 
 output "Container_name" {
-  value       = docker_container.nodered_container[0].name
+  value       = docker_container.nodered_container[*].name
   description = "This is container name for nodered_container."
 }
 
-output "Container_name2" {
-  value       = docker_container.nodered_container[1].name
-  description = "This is container name for nodered_container2."
-}
+# output "Container_name2" {
+#   value       = docker_container.nodered_container[1].name
+#   description = "This is container name for nodered_container2."
+# }
 
 output "IP_address_nodered-container" {
   value       = join(":", [docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external])
   description = "This is an IP address and external port of the container."
 }
 
-output "IP_address_nodered-container2" {
-  value       = join(":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external])
-  description = "This is an IP address and external port of the container."
-}
+# output "IP_address_nodered-container2" {
+#   value       = join(":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external])
+#   description = "This is an IP address and external port of the container."
+# }
 
